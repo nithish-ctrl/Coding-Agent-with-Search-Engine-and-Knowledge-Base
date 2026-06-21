8d27bbc1be4806e8230e5f2d471094d76b9ba174
Nithish <nithish221b@gmail.com>
2026-06-21 13:14:37 +0530

    Adding prompt response loop

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from typing import TypedDict, Sequence, Annotated
 from langchain_core.messages import BaseMessage, HumanMessage, SystemMessage, AIMessage
-from langgraph.graph import StateGraph, START, END, add_messages
+from langchain_core.messages import trim_messages
+from langgraph.graph import StateGraph, START, END, add_messages, MessagesState
 from langgraph.prebuilt import ToolNode
+from langgraph.checkpoint.memory import MemorySaver 
 from Tools import Search_engine, wiki_knowledge_base, results_log, To_do, Notes_tool, Notes_from_Documents, Resume_Analyzer
 from dotenv import load_dotenv
 # from langchain_google_genai import ChatGoogleGenerativeAI 
@@ -15,16 +17,27 @@ llm = ChatMistralAI(model_name="mistral-medium-3-5")
 Tools = [Search_engine, wiki_knowledge_base, results_log, To_do, Notes_tool, Notes_from_Documents, Resume_Analyzer]
 llm = llm.bind_tools(tools=Tools)
 
-class AgentState(TypedDict):
-    messages : Annotated[Sequence[BaseMessage], add_messages]
+checkpointer = MemorySaver()
 
+class AgentState(MessagesState):
+    # messages : Annotated[Sequence[BaseMessage], add_messages] # this is the same as just using MessageState instead of TypedDict
+    pass
 
 def Process_agent(state : AgentState) -> AgentState:
     system_prompt = System_prompt
+
+    trimmed_history = trim_messages(
+                state["messages"],
+                strategy = "last",
+                token_counter="approximate",
+                max_tokens=1000,
+                start_on="human",
+                end_on=("human", "tool")
+                )
     
     combined_messages = [
     SystemMessage(content=system_prompt),
-    *state["messages"]
+    *trimmed_history
     ]
     
     response = llm.invoke(combined_messages)
@@ -60,31 +73,40 @@ graph.add_conditional_edges(
     }
 )
 graph.add_edge("ToolNode", "AgentCall")
-agent = graph.compile()
+agent = graph.compile(checkpointer=checkpointer)
 
 def run_agent():
     print("______________________________________________________________________________________________________________")
-    user_input = input("Enter the prompt : ")
-    inputs = {"messages": [("user", user_input)]}
 
-    for output in agent.stream(inputs, stream_mode="updates"):  # type: ignore
+    config = {"configurable": {"thread_id": "session_1"}}
+
+    while True : 
+        user_input = input("Enter the prompt (or type exit or quit): ")
         
-        if "AgentCall" in output:
-            message = output["AgentCall"]["messages"][0]
+        if user_input.lower() == "quit" or user_input.lower() == "exit" : 
+            print(f"Alright, Byee until you call me back..See ya !!")
+            break
+
+        inputs = {"messages": [("user", user_input)]}
+
+        for output in agent.stream(inputs,config=config, stream_mode="updates"):  # type: ignore
             
-            if message.tool_calls:
-                for tool_call in message.tool_calls:
-                    print(f"\n[LLM]: Searching Tools for: \"{tool_call['args'].get('query')}\"")
+            if "AgentCall" in output:
+                message = output["AgentCall"]["messages"][0]
+                
+                if message.tool_calls:
+                    for tool_call in message.tool_calls:
+                        print(f"\n[LLM]: Searching Tools for: \"{tool_call['args'].get('query')}\"")
+                
+                elif message.content:
+                    #print(f"\n[LLM]: {message.content[0]['text']}") This is extract the text from Google Gemini Model
+                    print(f'LLM : {message.content}')
             
-            elif message.content:
-                #print(f"\n[LLM]: {message.content[0]['text']}") This is extract the text from Google Gemini Model
-                print(f'LLM : {message.content}')
+            elif "ToolNode" in output:
+                tool_message = output["ToolNode"]["messages"][0]
+                print(f"\n[Tools]: Pretty much done, Sending data back to LLM...")
         
-        elif "ToolNode" in output:
-            tool_message = output["ToolNode"]["messages"][0]
-            print(f"\n[Tools]: Found search results. Sending data back to LLM...")
-    
-    print("___________________________________________________________________________________________________________________")
+        print("___________________________________________________________________________________________________________________")
 
 if __name__ == "__main__":
     run_agent()
